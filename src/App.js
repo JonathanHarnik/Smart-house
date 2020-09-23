7e893c6fa5bbbf868e9ce8fdaba4dd8dd59690ff
Jonathan <jonathan.harnik@gmail.com>
2020-09-23 14:56:21 +0300

    Smart house complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+
 import {BrowserRouter as Router,Switch, Route, Link} from 'react-router-dom';
 import HomePage from './Components/HomePage.js';
 import AddRooms from './Components/AddRooms.js';
