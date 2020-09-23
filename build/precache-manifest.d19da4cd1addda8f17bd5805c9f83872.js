b40d67e966088016f08237ce051c5c366445feec
Jonathan <jonathan.harnik@gmail.com>
2020-09-23 15:45:30 +0300

    gh-pages

---
+++ build/precache-manifest.d19da4cd1addda8f17bd5805c9f83872.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515c3f85676cfbbf2abd387e33897328",
+    "url": "/Smart-house/index.html"
+  },
+  {
+    "revision": "8953379efd129319f4e2",
+    "url": "/Smart-house/static/css/main.2fa017f0.chunk.css"
+  },
+  {
+    "revision": "8ea939fc0a67cd54d897",
+    "url": "/Smart-house/static/js/2.9a7161e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Smart-house/static/js/2.9a7161e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8953379efd129319f4e2",
+    "url": "/Smart-house/static/js/main.2ad44a9e.chunk.js"
+  },
+  {
+    "revision": "c24d4a6cd381683d5161",
+    "url": "/Smart-house/static/js/runtime-main.701da2c6.js"
+  },
+  {
+    "revision": "92cb6c5665215b17b3d200f5daa1170d",
+    "url": "/Smart-house/static/media/SH_background.92cb6c56.jpg"
+  },
+  {
+    "revision": "bd47fdb53a16e40aff804ddff2969205",
+    "url": "/Smart-house/static/media/Smart-Home-addRoom.bd47fdb5.jpg"
+  },
+  {
+    "revision": "cf4210e9191e3ad91903a3308e96da8e",
+    "url": "/Smart-house/static/media/addRoomDiv.cf4210e9.jpg"
+  },
+  {
+    "revision": "44b3c7e10e20689386df9716b7584803",
+    "url": "/Smart-house/static/media/room.44b3c7e1.jpg"
+  }
+]);
